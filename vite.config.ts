abcce620e02398f40a966da4c7c4cb4993b3cbaa
Dede Fuji Abdul <defuj.official@gmail.com>
2025-08-19 10:19:45 +0700

    fix: update base path in Vite configuration to root

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sadigit-panel',
+  base: '/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
